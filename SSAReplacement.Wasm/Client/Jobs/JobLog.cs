c63d149de4227c4d2623ce96c566b48676cdf1a1
Zach-Richling <zwrdude@gmail.com>
2026-03-07 17:29:00 -0600

    Add job run detail page with console like view of job logs

---
+++ SSAReplacement.Wasm/Client/Jobs/JobLog.cs
@@ -0,0 +1,10 @@
+﻿namespace SSAReplacement.Wasm.Client.Jobs;
+
+public class JobLog
+{
+    public int Id { get; set; }
+    public int JobRunId { get; set; }
+    public DateTime LogDate { get; set; }
+    public string LogType { get; set; } = "";
+    public string Content { get; set; } = "";
+};
